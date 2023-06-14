db82ac8208abc5f2ea9b9df93adadef3bcedfd4b
thomasRLachance <thomasatulox@gmail.com>
2023-06-14 09:16:26 -0400

    Update page calendrier

--- codereact/src/Pages/PageCalendrier/Index.js
+++ codereact/src/Pages/PageCalendrier/Index.js
@@ -1,29 +1,40 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router";
 import HeaderConnected from "../../Utils/Component/headerConnected";
 import { Grid, Paper, Typography } from "@mui/material";
+import axios from "axios";
 
 export default function PageCalendrier() {
-  const getLocation = (locationId) => {
-    if (locationId === "1") {
-      return "Bistro Boudreau";
-    } else if (locationId === "2") {
-      return "Caféteria Comme Chez nous";
-    } else if (locationId === "3") {
-      return "Administration";
-    }
-  };
 
-  const user = {
-    name: sessionStorage.getItem("name"),
-    location: getLocation(sessionStorage.getItem("location")),
-  };
+  const [user, setUser] = useState({
+    userId: 1,
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    privileges: "",
+    locationId: "",
+  });
+
+
+  useEffect(() => {
+    //Get user
+    axios
+      .get(`http://localhost:3000/users/${sessionStorage.getItem("username")}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
-      {user.location === undefined && <Navigate replace to="/Erreur" />}
-      <HeaderConnected location={user.location} />
+      {user.locationId === undefined && <Navigate replace to="/Erreur" />}
+      <HeaderConnected user={user} />
       <Grid container justifyContent="center" alignItems="center">
         <Paper
           elevation={3}
